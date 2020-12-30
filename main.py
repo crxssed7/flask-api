f7e78d17e8c2a116be36ddf9d706a37cb280343a
Tanveer Najib <48864420+crxssed7@users.noreply.github.com>
2020-12-30 21:14:17 +0000

    Setup db for integration

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video")
-video_put_args.add_argument("views", type=int, help="Views of the video")
-video_put_args.add_argument("likes", type=int, help="Likes of the video")
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
-videos = {}
+resource_fields = {
+    'id': fields.Integer, 
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        return videos[video_id]
+        result = VideoModel.query.get(id=video_id)
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        
-        return {}
+        args = video_put_args.parse_args()
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    def delete(self, video_id):
+        abort_if_video_id_not_found(video_id)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,5 +2,24 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+# Make a record
+response = requests.put(BASE + "video/1", {"name":"video name", "views":100, "likes": 10})
+print(response.json())
+
+input()
+
+# Show that record
+response = requests.get(BASE + "video/1")
+print(response.json())
+
+input()
+
+# Delete that record
+response = requests.delete(BASE + "video/1")
+print(response)
+
+input()
+
+## Show that 
+response = requests.get(BASE + "video/1")
 print(response.json())
