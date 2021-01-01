2039ff9243a081c731a8d0993f383efd157a132b
Tanveer Najib <48864420+crxssed7@users.noreply.github.com>
2021-01-01 14:33:18 +0000

    Finished Basics
    Finished the basics of a flask api following this tutorial: https://www.youtube.com/watch?v=GMppyAPbLYk&t=3602s&ab_channel=TechWithTim

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ class VideoModel(db.Model):
     likes = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"Video(name={name}, views={views}, likes={likes})"
+        return f"Video(name={self.name}, views={self.name}, likes={self.likes})"
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
+
+video_patch_args = reqparse.RequestParser()
+video_patch_args.add_argument("name", type=str, help="Name of the video")
+video_patch_args.add_argument("views", type=int, help="Views of the video")
+video_patch_args.add_argument("likes", type=int, help="Likes of the video")
 
 resource_fields = {
     'id': fields.Integer, 
@@ -31,20 +36,49 @@ resource_fields = {
 class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
-        result = VideoModel.query.get(id=video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="No video found")
         return result
 
     @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video ID already exists")
         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
         db.session.add(video)
         db.session.commit()
         return video, 201
 
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_patch_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+
+        if not result:
+            abort(404, message="No video found")
+        
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+        
+        db.session.commit()
+
+        return result
+
     def delete(self, video_id):
-        abort_if_video_id_not_found(video_id)
-        del videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="No video found")
+        
+        db.session.delete(result)
+        db.session.commit()
+
         return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,24 +2,10 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# Make a record
-response = requests.put(BASE + "video/1", {"name":"video name", "views":100, "likes": 10})
-print(response.json())
-
-input()
-
-# Show that record
-response = requests.get(BASE + "video/1")
-print(response.json())
-
-input()
-
-# Delete that record
-response = requests.delete(BASE + "video/1")
+response = requests.delete(BASE + "video/3")
 print(response)
 
 input()
 
-## Show that 
-response = requests.get(BASE + "video/1")
+response = requests.get(BASE + "video/3")
 print(response.json())
